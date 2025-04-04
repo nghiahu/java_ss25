cb3d9ef5cf6366ee819438192019986fc2f72325
CanhVeTaiGia <nghia120425@gmail.com>
2025-04-04 10:17:36 +0700

    update

--- src/baitap/Bai4/Main.java
+++ src/baitap/Bai4/Main.java
@@ -2,6 +2,9 @@ package baitap.Bai4;
 
 public class Main {
     public static void main(String[] args) {
-
+        Adapter adapter1 = new Adapter();
+        adapter1.play("Mp4", "videoMp4");
+        adapter1.play("Vlc", "audioVLc");
+        adapter1.play("Mp3", "audioMp3");
     }
 }
